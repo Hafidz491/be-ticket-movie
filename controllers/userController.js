ef2324c306da6ba17b6edc75a56ecf03a877fdc7
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-29 14:48:34 +0700

    build login user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 class UserController {
   static async signupUser(req, res) {
@@ -7,19 +9,58 @@ class UserController {
       if (!req.body.name || !req.body.email || !req.body.password) {
         return res.status(400).json({ msg: "Please enter all fields" });
       }
+
+      //   check if user exists
       const checking = await User.findOne({ email: req.body.email });
       if (checking) {
         return res.status(400).json({ msg: "Email already exists" });
       }
 
+      // Hash Password
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(req.body.password, salt);
+
       const user = await User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
       });
 
       const newUser = await user.save();
       res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  static async loginUser(req, res) {
+    try {
+      // validation input form
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res.status(400).json({ msg: "Please enter all fields!" });
+      }
+
+      //   check if user exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "User does not exist!" });
+      }
+
+      //   checking valid password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Password!" });
+      }
+
+      //   make jwt token
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.status(201).json({
+        token,
+        user: { id: user._id, name: user.name, email: user.email },
+      });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require("express");
 const UserController = require("../controllers/userController");
 const User = require("../models/User");
 var router = express.Router();
-let Users = [];
 
 /* GET users listing. */
 router.post("/signup", UserController.signupUser);
+router.post("/auth/login", UserController.loginUser);
 
 module.exports = router;
