4d28860a9707727d86d774a58741462db2f2e603
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-31 09:13:54 +0700

    build detail movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -25,6 +25,24 @@ class MovieController {
       res.status(500).json({ message: error.message });
     }
   }
+
+  static async detailMovie(req, res) {
+    try {
+      // get ID
+      const id = req.params.id;
+
+      const movie = await Movie.findById(id);
+
+      // checking movie id
+      if (!id) {
+        res.status(404).json({ message: "Movie not found" });
+      }
+
+      res.status(201).json(movie);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 }
 
 module.exports = MovieController;
