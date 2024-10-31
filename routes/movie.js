aa2002bd12cc500c311eee1a7e605a5a6e9d4823
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-10-31 09:17:10 +0700

    route detail movie

--- routes/movie.js
+++ routes/movie.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", MovieController.addMovie);
 router.get("/all-movie", MovieController.getMovies);
+router.get("/detail/:id", MovieController.detailMovie);
 
 module.exports = router;
